c9a97ab2d5f48ce6d123f43841caa4643be82f38
habib-sc <habib97.cs@gmail.com>
2022-06-24 03:11:06 +0600

    showing notes form mongodb

--- src/components/NoteCard/NoteCard.js
+++ src/components/NoteCard/NoteCard.js
@@ -1,15 +1,16 @@
 
-const NoteCard = () => {
+const NoteCard = (props) => {
+    const {title, description} = props.note
     return (
         <div className="shadow-xl rounded p-4 border">
             <div className="flex items-center justify-between">
-                <h3 className="text-xl font-semibold pb-1 mb-1">Title text goes here</h3>
+                <h3 className="text-xl font-semibold pb-1 mb-1">{title}</h3>
                 <div className="flex gap-4">
                     <button className="border px-2 rounded bg-indigo-100 text-indigo-500">Edit</button>
                     <button className="border px-2 rounded bg-red-100 text-red-500">Delete</button>
                 </div>
             </div>
-            <p>Description goes here</p>
+            <p>{description}</p>
         </div>
     );
 };
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,7 +1,18 @@
+import { useQuery } from 'react-query';
 import NoteCard from "../NoteCard/NoteCard";
 
 const Notes = () => {
+
+    // Getting notes 
+    const {data: notes, isLoading, refetch } = useQuery('notes', () => fetch('http://localhost:5000/todos').then(res=> res.json()));
+
+    if (isLoading){
+        return <p>Loading...</p>
+    }
+
     return (
+
+        
         <div>
 
             <div className="flex justify-center items-center mt-12 mb-8">
@@ -11,9 +22,9 @@ const Notes = () => {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                <NoteCard></NoteCard>
-                <NoteCard></NoteCard>
-                <NoteCard></NoteCard>
+                {
+                    notes.map(note => <NoteCard key={note._id} note={note}></NoteCard>)
+                }
             </div>
         </div>
     );
