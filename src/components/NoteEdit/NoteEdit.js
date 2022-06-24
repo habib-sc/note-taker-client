97eef74b21fe6c831014c2e3222d183bb74ed372
habib-sc <habib97.cs@gmail.com>
2022-06-24 12:53:34 +0600

    edit note heading change

--- src/components/NoteEdit/NoteEdit.js
+++ src/components/NoteEdit/NoteEdit.js
@@ -44,10 +44,10 @@ const NoteEdit = () => {
 
 
     return (
-        <div className="mt-10">
+        <div className="container mx-auto px-4 mt-10">
             <form onSubmit={handleNoteEdit}>
                 <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-4 flex flex-col mx-auto w-full mt-10 md:mt-0 relative shadow-md">
-                    <h2 className="text-gray-900 text-lg md:text-2xl mb-3 text-center font-medium title-font">Edit This Note</h2>
+                    <h2 className="text-gray-900 text-lg md:text-2xl mb-3 text-center font-medium title-font">EDIT THIS NOTE</h2>
                     <input type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} placeholder="Note Title" className="w-full mb-3 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                     <textarea name="description" value={description} onChange={e => setDescription(e.target.value)} placeholder="Note Description" className="w-full mb-3 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-20 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Update</button>
